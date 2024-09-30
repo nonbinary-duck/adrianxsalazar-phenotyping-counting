8a1e785a898b23a4f7d20a6ed410d753077b93b4
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-10-01 00:59:38 +0100

    Adjusted size so the legend isn't enormous

--- code/utils/plot_metrics.py
+++ code/utils/plot_metrics.py
@@ -3,24 +3,29 @@ import seaborn as sns
 import pandas as pd
 
 
-met_df: pd.DataFrame = pd.read_pickle("metrics.pkl");
+def plot_metrics():
+    met_df: pd.DataFrame = pd.read_pickle("metrics.pkl");
 
-met_df = met_df.assign(epoch=met_df.index+1);
+    met_df = met_df.assign(epoch=met_df.index+1);
 
-# Fix incorrect metric reporting in from old code
-print(met_df.columns.to_list())
-if ("val_mse" in met_df.columns.to_list()): met_df = met_df.rename( columns= { "val_mse": "val_mae" } );
+    # Fix incorrect metric reporting in from old code
+    print(met_df.columns.to_list())
+    if ("val_mse" in met_df.columns.to_list()): met_df = met_df.rename( columns= { "val_mse": "val_mae" } );
 
 
-# met_df["epoch"] += 99;
-print(met_df);
+    # met_df["epoch"] += 99;
+    print(met_df);
 
-met_mdf = met_df.melt("epoch", var_name="metric", value_name="value")
+    met_mdf = met_df.melt("epoch", var_name="metric", value_name="value")
 
-sns.set_theme();
-sns.regplot(data=met_df.where(met_df.epoch > 10), x="epoch", y="val_mae", marker="x", ax=plt.subplot(2,2,1));
-sns.regplot(data=met_df.where(met_df.epoch > 10), x="epoch", y="train_loss", marker="x", ax=plt.subplot(2,2,2));
-sns.lineplot(data=met_mdf.where(met_mdf.epoch > 10), x="epoch", y="value", hue="metric", palette="flare", ax=plt.subplot(2,2,3));
-sns.lineplot(data=met_mdf.where(met_mdf.epoch < 10), x="epoch", y="value", hue="metric", palette="flare", ax=plt.subplot(2,2,4));
-plt.savefig("metrics.png", dpi=300);
-plt.show();
+    sns.set_theme();
+    plt.figure(figsize=(16,9), dpi=72);
+    sns.regplot(data=met_df.where(met_df.epoch > 10), x="epoch", y="val_mae", marker="x", ax=plt.subplot(2,2,1));
+    sns.regplot(data=met_df.where(met_df.epoch > 10), x="epoch", y="train_loss", marker="x", ax=plt.subplot(2,2,2));
+    sns.lineplot(data=met_mdf.where(met_mdf.epoch > 10), x="epoch", y="value", hue="metric", palette="flare", ax=plt.subplot(2,2,3));
+    sns.lineplot(data=met_mdf.where(met_mdf.epoch < 10), x="epoch", y="value", hue="metric", palette="flare", ax=plt.subplot(2,2,4));
+    plt.savefig("metrics.png", dpi=300);
+    plt.show();
+
+
+plot_metrics();
