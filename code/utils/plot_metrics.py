287c1b7733b626b8763a1e7d2e5dc206dd4e5de8
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-09-30 12:01:48 +0100

    New visualisation code

---
+++ code/utils/plot_metrics.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+met_df: pd.DataFrame = pd.read_pickle("metrics.pkl");
+
+met_df = met_df.assign(epoch=met_df.index+1);
+# met_df["epoch"] += 99;
+print(met_df);
+
+met_mdf = met_df.melt("epoch", var_name="metric", value_name="value")
+
+sns.set_theme();
+sns.regplot(data=met_df.where(met_df.epoch > 10), x="epoch", y="val_mse", ax=plt.subplot(2,2,1));
+sns.regplot(data=met_df.where(met_df.epoch > 10), x="epoch", y="train_loss", ax=plt.subplot(2,2,2));
+sns.lineplot(data=met_mdf.where(met_mdf.epoch > 10), x="epoch", y="value", hue="metric", palette="flare", ax=plt.subplot(2,2,3));
+sns.lineplot(data=met_mdf.where(met_mdf.epoch < 10), x="epoch", y="value", hue="metric", palette="flare", ax=plt.subplot(2,2,4));
+plt.show();
