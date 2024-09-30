9e5eac48cb28bd5808a9bfe5eb9c09a72ea3fac3
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2024-09-30 12:46:50 +0100

    Added per-class stats and fixed Qt error by getting rid of Qt backend

--- code/models/can/test.py
+++ code/models/can/test.py
@@ -15,6 +15,8 @@ import json
 import matplotlib.image as mpimg
 from matplotlib import pyplot as plt
 from matplotlib import cm as c
+import matplotlib
+matplotlib.use('Agg')
 plt.style.use('classic')
 
 import shutil
@@ -98,6 +100,7 @@ for img_path in img_paths:
     # Swap from cxy to xyc
     visden = np.transpose(visden, (1, 2, 0));
     # Plot it
+    
     plt.imshow(visden);
     # Save it
     plt.gca().set_axis_off()
@@ -105,6 +108,8 @@ for img_path in img_paths:
     plt.margins(0,0)
     plt.savefig(os.path.join(vis_path, plain_file)+"_output.png",bbox_inches='tight', pad_inches = 0,dpi=300)
     plt.close()
+
+    
 
     # Save individually the channels
     for i in range(3):
@@ -136,7 +141,7 @@ for img_path in img_paths:
     plt.close()
     
     # This code needs to be replaced by automatically rendering and linking the boxed images into the dataset
-    plt.imshow(mpimg.imread( os.path.join("../boxed", img_path + ".boxed.jpg")))
+    plt.imshow(mpimg.imread( os.path.join("boxed_imgs", img_path + ".boxed.jpg")))
     plt.gca().set_axis_off()
     plt.axis('off')
     plt.margins(0,0)
@@ -148,7 +153,11 @@ for img_path in img_paths:
     pred_sum = den.sum();#density_1.sum()+density_2.sum()+density_3.sum()+density_4.sum()
     dictionary_counts[plain_file]={ "pred": float(pred_sum), "gt": float(np.sum(groundtruth)) };#round(abs(pred_sum),3)
 
-    print (f"For image {plain_file}\n  predicted: {pred_sum}, gt: {np.sum(groundtruth)}\n");
+    print (f"For image {plain_file}");
+    for i in range(3):
+        print(f"  c_{i}: predicted: {den[i, :, :].sum()}, gt: {np.sum(groundtruth[:, :, i])}\n");
+
+    print(f"  c_ALL: predicted: {pred_sum}, gt: {np.sum(groundtruth)}");
 
     pred.append(pred_sum)
 
